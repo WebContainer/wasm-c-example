ee2cc068a4badb3f744f97b7eccb947977a14b9a
Jacob Groundwater <groundwater@gmail.com>
2018-05-15 20:55:27 -0700

    snapshot

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void trace(int);
 void uppercase(char * str);
 
 int testOpenReadClose(char * arg) {
-    print("testOpenReadClose");
+    printf("testOpenReadClose\n");
 
     int fd = open(arg, O_RDONLY);
 
@@ -37,22 +38,55 @@ int testOpenReadClose(char * arg) {
 
     write(1, "HI\n", 3);
 
-    print(buf);
+    printf("%s\n", buf);
 
     close(fd);
 
     return 0;
 }
 
-void printFloat() {
-    // THIS CURRENTLY EXPLODES
-    // printf("%f", 1.2);
+void printfTypes() {
+    printf("char: %c\n", 'a');
+
+    printf("unsigned int: %u\n", 123123U);
+    printf("unsigned long: %lu\n", 123123UL);
+
+    printf("int: %d\n", 123);
+    printf("long: %ld\n", 1223424343L);
+    printf("long: %lld\n", 24234323LL);
+    
+    printf("float: %f\n", 123.234f);
+    printf("double: %f\n", 123.234);
+    printf("long double: %Lf\n", 123412341.2341234L);
+
+    printf("hex: %x\n", 255);
+
+    printf("string: %s\n", "Hello World");
+}
+
+void testFork() {
+    // none of this works
+
+    int pid = fork();
+    if (pid < 0) {
+        print("FORK FAILED");
+        return;
+    }
+
+    if (pid > 0) {
+        print("CHILD");
+
+        exit(0);
+    } else {
+        print("PARENT");
+    }
 }
 
 PLEASE_KEEP_THIS_METHOD
 int main(int argc, char ** argv) {
     testOpenReadClose(argv[1]);
-    printFloat();
+    printfTypes();
+    testFork();
 
     return 0;
 }
